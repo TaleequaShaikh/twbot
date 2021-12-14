f25c5c722427d12545c998b216d5c47bded50562
TaleequaShaikh <90542447+TaleequaShaikh@users.noreply.github.com>
2021-12-14 08:52:26 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,9 @@
 import tweepy
 import time
-consumer_key = 'PXRZkFBsot70MtByCe9lWApif'
-consumer_secret = 'sp67sK8loc8QkdZmvM1636dd63TV3SBgk8MCvpDqpOUHeL78Cp'
-key = '1429310907545387008-MRXkvMZ34wkHb8hB0n5dlJXeCJmMW9'
-secret = 'i2r9R4pDQRUYS0bMh6qF0FKpCB200dViC1EAt0casuWJs'
+consumer_key = 'PXRZkFBsot70MtByCe9lW'
+consumer_secret = 'sp67sK8loc8QkdZmvM1636dd63TV3SBgk8MCvpDqpOUHeL'
+key = '1429310907545387008-MRXkvMZ34wkHb8hB0n5dlJXeCJ'
+secret = 'i2r9R4pDQRUYS0bMh6qF0FKpCB200dViC1EAt0cas'
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(key, secret)
 
